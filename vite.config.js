d65bdd17843c72247c3498d2f72d3017a6514121
kirbyliu99M <kirby99liu@gmail.com>
2026-05-02 00:36:32 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: '/your-repo-name/', // Uncomment and update this if deploying to a GitHub Pages subpath
+})
